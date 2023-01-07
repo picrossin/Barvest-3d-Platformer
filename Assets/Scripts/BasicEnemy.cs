93ff8579a4293cca03ea7f7e508812f807d2fe08
camden-obertop <camden.obertop@gmail.com>
2023-01-06 23:29:42 -0600

    :bug: Added some enemy movement nav mesh stuff

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -5,19 +5,77 @@ using UnityEngine.AI;
 
 public class BasicEnemy : MonoBehaviour
 {
-    private NavMeshAgent m_Enemy;
-    [SerializeField] private Transform m_Target;
-    [SerializeField] private Vector3[] M_corners;
+    // Editor values
+    [SerializeField] private List<Vector3> m_PathPositions;
 
-    // Start is called before the first frame update
+    // Private values
+    private NavMeshAgent m_Enemy;
+    private int m_CurrentDestinationIndex;
+    private bool m_Turning = false;
+
+    // AddPositionToPath is used by an editor script to populate the m_PathPositions List
+    public void AddPositionToPath()
+    {
+        m_PathPositions.Add(this.transform.position);
+    }
+
     void Start()
     {
         m_Enemy = GetComponent<NavMeshAgent>();
+        m_Enemy.destination = m_PathPositions[m_CurrentDestinationIndex];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        m_Enemy.SetDestination(m_Target.position);
+        // Draw Debug Ray
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 1;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        // Move
+        MoveEnemy();
+    }
+
+    void MoveEnemy()
+    {
+        // Check if we've reached the destination
+        // https://answers.unity.com/questions/324589/how-can-i-tell-when-a-navmesh-has-reached-its-dest.html
+        if (m_Turning) return;
+        if (m_Enemy.pathPending) return;
+        if (m_Enemy.remainingDistance > m_Enemy.stoppingDistance) return;
+        if (m_Enemy.hasPath || m_Enemy.velocity.sqrMagnitude != 0f) return;
+
+        Debug.Log("Destination Reached!");
+        StartCoroutine(SmoothTurn());
+    }
+
+    IEnumerator SmoothTurn()
+    {
+        Debug.Log("Turn Initiated");
+        // Start turning
+        m_Turning = true;
+
+        // Update destination index and get the next destination position
+        m_CurrentDestinationIndex = (m_CurrentDestinationIndex + 1) % m_PathPositions.Count;
+        Vector3 nextDestination = m_PathPositions[m_CurrentDestinationIndex];
+
+        // Get the starting and target rotation for the slerp
+        Quaternion startingRotation = transform.rotation;
+        Vector3 direction = (nextDestination - transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        float timeTaken = 0f;
+        float timeToTake = .5f;
+        while (timeTaken < timeToTake)
+        {
+            transform.rotation = Quaternion.Slerp(startingRotation, targetRotation, timeTaken / timeToTake);
+            timeTaken += Time.deltaTime;
+            yield return null;
+        }
+
+        // Set the nav mesh agent destination
+        m_Enemy.destination = nextDestination;
+        
+        // Stop turning
+        m_Turning = false;
     }
 }
