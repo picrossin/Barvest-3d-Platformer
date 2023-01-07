dda21111971d249da28d26a478a032cd4d784bd0
Jaden Goter <jadengoter@gmail.com>
2023-01-07 11:03:52 -0800

    :train: Movement and camera fixes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,7 @@ public class CameraController : MonoBehaviour
 {
 	[SerializeField] private Transform m_Player;
 	[SerializeField] private float m_LookSensitivity = 1f;
+	[SerializeField] private float m_CameraVerticalFollowOffset = 0.25f;
 
 	private void Start()
 	{
@@ -11,31 +12,37 @@ public class CameraController : MonoBehaviour
 		Cursor.lockState = CursorLockMode.Locked;
 	}
 
-    private void FixedUpdate()
+	private void LateUpdate()
+	{
+		Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * 
+		                (Time.fixedDeltaTime * m_LookSensitivity);
+        
+		// Horizontal rotation
+		transform.localRotation *= Quaternion.AngleAxis(input.x, Vector3.up);
+
+		// Vertical rotation
+		transform.localRotation *= Quaternion.AngleAxis(-input.y, Vector3.right);
+        
+		Vector3 angles = transform.localEulerAngles;
+		angles.z = 0;
+
+		float angle = transform.localEulerAngles.x;
+
+		if (angle > 180f && angle < 360f)
+		{
+			angles.x = 360f;
+		}
+		else if (angle < 180f && angle > 40f)
+		{
+			angles.x = 40f;
+		}
+
+		transform.localEulerAngles = angles;
+	}
+
+	private void FixedUpdate()
     {
-	    Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * 
-	                    (Time.fixedDeltaTime * m_LookSensitivity);
-        
-	    // Horizontal rotation
-	    transform.localRotation *= Quaternion.AngleAxis(input.x, Vector3.up);
-
-	    // Vertical rotation
-	    transform.localRotation *= Quaternion.AngleAxis(-input.y, Vector3.right);
-        
-	    Vector3 angles = transform.localEulerAngles;
-	    angles.z = 0;
-
-	    float angle = transform.localEulerAngles.x;
-
-	    if (angle > 180f && angle < 320f)
-	    {
-		    angles.x = 320f;
-	    }
-	    else if (angle < 180f && angle > 40f)
-	    {
-		    angles.x = 40f;
-	    }
-
-	    transform.localEulerAngles = angles;
+	    if (m_Player != null)
+			transform.position = m_Player.position + Vector3.up * m_CameraVerticalFollowOffset;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     [Header("Dependencies")]
     [SerializeField] private Transform m_CameraRig;
     [SerializeField] private LayerMask m_GroundLayerMask;
+    [SerializeField] private PhysicMaterial m_noFriction;
+    [SerializeField] private PhysicMaterial m_HighFriction;
     
     [Header("Settings")]
     [SerializeField] private float m_MaxSpeed = 2f;
@@ -14,8 +16,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float m_TurnAnimSpeedFast = 2f;
     [SerializeField] private float m_JumpForce = 30f;
     [SerializeField] private float m_DoubleJumpForce = 5f;
+    [SerializeField] private float m_Friction = 1f;
 
     private Rigidbody m_Rigidbody;
+    private Collider m_Collider;
     
     private Quaternion m_LookRotation = Quaternion.identity;
     private Vector2 m_Input;
@@ -26,6 +30,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<Collider>();
     }
     
     private void Update()
@@ -38,11 +43,17 @@ public class PlayerController : MonoBehaviour
             {
                 m_Rigidbody.AddForce(Vector3.up * m_JumpForce, ForceMode.Impulse);
                 m_DoubleJumped = false;
+                
+                Vector3 oldVel = m_Rigidbody.velocity;
+                m_Rigidbody.velocity = new Vector3(oldVel.x, 0f, oldVel.z);
             }
             else if (!m_DoubleJumped)
             {
                 m_Rigidbody.AddForce(Vector3.up * m_DoubleJumpForce, ForceMode.Impulse);
                 m_DoubleJumped = true;
+                
+                Vector3 oldVel = m_Rigidbody.velocity;
+                m_Rigidbody.velocity = new Vector3(oldVel.x, 0f, oldVel.z);
             }
         }
     }
@@ -53,11 +64,21 @@ public class PlayerController : MonoBehaviour
         Vector3 relativeInput = Quaternion.Euler(0, m_CameraRig.eulerAngles.y, 0) * new Vector3(m_Input.x, 0f, m_Input.y);
 
         // Handle slopes
+        bool grounded = false;
         if (Physics.Raycast(transform.position + Vector3.up * 0.2f, Vector3.down, out RaycastHit hit, 0.3f, m_GroundLayerMask))
         {
+            grounded = true;
             transform.rotation =
                 Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hit.normal), hit.normal);
             relativeInput = Vector3.ProjectOnPlane(relativeInput, hit.normal);
+            
+            Debug.DrawRay(transform.position, relativeInput, Color.magenta);
+
+            if (hit.normal != Vector3.up)
+            {
+                m_Collider.material = m_Input.magnitude <= Mathf.Epsilon ? m_HighFriction : m_noFriction;
+            }
+            
             Debug.DrawRay(transform.position + Vector3.up * 0.2f, Vector3.down * 0.3f, Color.red);
         }
 
@@ -79,7 +100,7 @@ public class PlayerController : MonoBehaviour
         if (m_Input.magnitude > 0.05f)
             m_LookRotation = Quaternion.LookRotation(relativeInput, transform.up);
 
-        if (m_Rigidbody.velocity.magnitude > 0f)
+        if (rbXZVelocity.magnitude > 0f)
         {
             if (Quaternion.Angle(transform.rotation, m_LookRotation) < 100f)
             {
@@ -90,7 +111,11 @@ public class PlayerController : MonoBehaviour
                 transform.rotation = Quaternion.Slerp(transform.rotation, m_LookRotation, m_TurnAnimSpeedFast);
             }
         }
-
-        m_CameraRig.position = transform.position;
+        
+        // Friction
+        if (grounded && rbXZVelocity.magnitude > Mathf.Epsilon && m_Input.magnitude <= Mathf.Epsilon)
+        {
+            m_Rigidbody.AddForce(-rbXZVelocity * m_Friction);
+        }
     }
 }
