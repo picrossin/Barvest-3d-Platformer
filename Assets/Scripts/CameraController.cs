d3b890ce835e942cbdb7a1c76ff20fb4f20cd1d9
Jaden Goter <jadengoter@gmail.com>
2023-01-06 22:03:37 -0800

    :sob: Initial player movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,36 +12,33 @@ public class CameraController : MonoBehaviour
 		Cursor.lockState = CursorLockMode.Locked;
 	}
 
-	private void LateUpdate()
-    {
-        Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * 
-                        (Time.fixedDeltaTime * m_LookSensitivity);
-        
-        // Horizontal rotation
-        transform.localRotation *= Quaternion.AngleAxis(input.x, Vector3.up);
-
-        // Vertical rotation
-        transform.localRotation *= Quaternion.AngleAxis(-input.y, Vector3.right);
-        
-        Vector3 angles = transform.localEulerAngles;
-        angles.z = 0;
-
-        float angle = transform.localEulerAngles.x;
-
-        if (angle > 180f && angle < 340f)
-        {
-	        angles.x = 340f;
-        }
-        else if (angle < 180f && angle > 40f)
-        {
-	        angles.x = 40f;
-        }
-
-        transform.localEulerAngles = angles;
-    }
-
     private void FixedUpdate()
     {
+	    Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * 
+	                    (Time.fixedDeltaTime * m_LookSensitivity);
+        
+	    // Horizontal rotation
+	    transform.localRotation *= Quaternion.AngleAxis(input.x, Vector3.up);
+
+	    // Vertical rotation
+	    transform.localRotation *= Quaternion.AngleAxis(-input.y, Vector3.right);
+        
+	    Vector3 angles = transform.localEulerAngles;
+	    angles.z = 0;
+
+	    float angle = transform.localEulerAngles.x;
+
+	    if (angle > 180f && angle < 320f)
+	    {
+		    angles.x = 320f;
+	    }
+	    else if (angle < 180f && angle > 40f)
+	    {
+		    angles.x = 40f;
+	    }
+
+	    transform.localEulerAngles = angles;
+	    
 	    transform.position = m_Player.position;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float m_Speed = 2;
+    [Header("Dependencies")]
+    [SerializeField] private Transform m_CameraRig;
     
+    [Header("Settings")]
+    [SerializeField] private float m_Speed = 2;
+    [SerializeField] private float m_TurnAnimSpeed = 2f;
+
+    private Rigidbody m_Rigidbody;
+    
+    private Quaternion m_LookRotation = Quaternion.identity;
     private Vector2 m_Input;
 
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    
     private void Update()
     {
-        m_Input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        m_Input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
     
     private void FixedUpdate()
     {
-        transform.position += new Vector3(m_Input.x, 0, m_Input.y) * m_Speed;
+        Vector3 relativeInput = Quaternion.Euler(0, m_CameraRig.eulerAngles.y, 0) * new Vector3(m_Input.x, 0f, m_Input.y);
+
+        if (m_Input != Vector2.zero)
+        {
+            m_LookRotation = Quaternion.LookRotation(relativeInput, transform.up);
+        }
+        
+        m_Rigidbody.AddForce(relativeInput * m_Speed);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, m_LookRotation, m_TurnAnimSpeed);
     }
 }
